29b28a5b105ab077e5c7221700ce3049c08ee5ab
Enzo-GH <rm94182@estudante.fieb.edu.br>
2025-03-31 08:29:27 -0300

    component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,41 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('Ronaldo')
+  
+
+  const Formulario = (props) => {
+
+    const [nome, setNome] = useState('Ronaldo')
+
+    return (
+      <div>
+        <h3>{props.título}</h3>
+        <input className="nome" 
+          placeholder="Digite seu nome..." 
+          onChange={(e)=>{setNome(e.target.value)}}
+          name="nome" 
+          type="text" />
+        <button className="botao" onClick={
+          () => {
+            if(nome.length > 0)
+              alert(nome)
+            else
+              alert(props.mensagem)
+          }
+        }>          
+        CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
 
   return (
     <div>
       <h3>Pizzaria 2B</h3>
-      <input className="nome" 
-      placeholder="Digite seu nome..." 
-      onChange={(e)=>{setNome(e.target.value)}}
-      name="nome" 
-      type="text" />
-      <button className="botao" onClick={()=>alert(nome)}>
-        CLIQUE AQUI
-      </button>
+      <Formulario título = "Nome" mensagem = "Santos"/>
+      <Formulario título = "Email" mensagem = "Curintia"/>
+      <Formulario título = "CPF" mensagem = "Parmera"/>
+      <Formulario />
     </div>
   )
 }
